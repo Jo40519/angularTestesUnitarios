b212504cdd29ea8dba25ad4d8e2849ce0ca4ea56
João Vitor Nunes Lobato de Souza <84386921+Jo40519@users.noreply.github.com>
2022-08-20 11:20:57 -0300

    Teste

--- angularTesteUnitario/src/app/shared/baking/baking.component.spec.ts
+++ angularTesteUnitario/src/app/shared/baking/baking.component.spec.ts
@@ -25,4 +25,41 @@ describe('BakingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`(U) getSacar: should poupanca = 10`, () => {
+    expect(component.getSacar).toEqual(10)
+  });
+
+  it(`(U) getCarteira: should carteira = 50`, () => {
+    expect(component.getCarteira).toEqual(50)
+  });
+
+  it(`(U) sacar: sohuld transfer poupanca from carteira`, () => {
+    component.sacar('10');
+    fixture.detectChanges();
+    expect(component.getSacar).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  });
+
+  it('(U) sacar (): should transfer poupanca dont have string (isNan) or poupanca < value', () => {
+    expect(component.sacar('string')).not.toBeTruthy();
+    expect(component.sacar('100')).not.toBeTruthy();
+
+    expect(component.getSacar).toEqual(10)
+    expect(component.getCarteira).toEqual(50)
+  })
+
+  it(`(U) setDepositar: should transfer carteira from poupanca`, () => {
+    component.setDepositar('50');
+    expect(component.getSacar).toEqual(0);
+    expect(component.getCarteira).toEqual(60);
+  })
+
+  it('(U) setDepositar(): should transfer carteria dont have string (isNan) or carteira < value', () => {
+    expect(component.setDepositar('string')).not.toBeTruthy();
+    expect(component.setDepositar('100')).not.toBeTruthy();
+
+    expect(component.getSacar).toEqual(10);
+    expect(component.getCarteira).toEqual
+  })
 });
--- angularTesteUnitario/src/app/shared/baking/baking.component.ts
+++ angularTesteUnitario/src/app/shared/baking/baking.component.ts
@@ -7,17 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BakingComponent implements OnInit {
 
+  private poupanca: number = 10;
+  private carteira: number = 50;
+
   constructor() { }
 
   ngOnInit() {
   }
 
-
-  sacar(valor: string): number{
+get getSacar(): number {
+  return this.poupanca;
+}
+  sacar(valor: string): number | undefined{
     const sacar = Number(valor)
 
-    console.log(sacar)
+    if(isNaN(sacar) || this.poupanca < sacar){
+      return
+    }
 
-    return sacar
+    this.poupanca -= sacar;
+
+    return this.carteira += sacar
+  }
+
+  get getCarteira() :number {
+  return this.carteira;
+  }
+  setDepositar(valor: string): number | undefined {
+    const depositar = Number(valor)
+    if(isNaN(depositar) || this.carteira < depositar) {
+      return;
+    }
+    this.carteira -= depositar
+    return this.poupanca += depositar
   }
 }
